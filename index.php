73ff7caacc0932e413ecc5ed81ba5fab595bf839
giovannitamburini <giovatambu@gmail.com>
2023-05-03 17:11:18 +0200

    added bonus 1

--- index.php
+++ index.php
@@ -33,7 +33,11 @@ class Movie
     public static $director = "Christopher Nolan";
 
     // definisco un costruttore (per rendere obbligatorio l'indicazione dei dati)
-    function __construct(string $title, string $genre, int $year, string $mainCharacter, int $price)
+    // costruttore base, senza un array
+    // function __construct(string $title, string $genre, int $year, string $mainCharacter, int $price)
+
+    // costruttore con array per il genere
+    function __construct(string $title, array $genre, int $year, string $mainCharacter, int $price)
     {
         $this->title = $title;
         $this->genre = $genre;
@@ -72,20 +76,29 @@ class Movie
 }
 
 // istanzio una nuova entità, cioè un nuovo oggetto
-$interstellar = new Movie("Interstellar", "science fiction", 2014, "Matthew McConaughey", 15);
+// versione senza array
+// $interstellar = new Movie("Interstellar", "science fiction", 2014, "Matthew McConaughey", 15);
+//versione con array
+$interstellar = new Movie("Interstellar", ["science fiction", "adventure"], 2014, "Matthew McConaughey", 15);
 
 // istanzio una nuova entità, cioè un nuovo oggetto
-$inception = new Movie("Inception", "science fiction", 2010, "Leonardo DiCaprio", 10);
+//versione senza array
+// $inception = new Movie("Inception", "science fiction", 2010, "Leonardo DiCaprio", 10);
+// versione con array
+$inception = new Movie("Inception", ["science fiction", "action"], 2010, "Leonardo DiCaprio", 10);
+
 
 // istanzio una nuova entità, cioè un nuovo oggetto
-$oppenheimer = new Movie("Oppenheimer", "biographical", 2023, "Cillian Murphy", 20);
+// versione senza array
+// $oppenheimer = new Movie("Oppenheimer", "biographical", 2023, "Cillian Murphy", 20);
+// versione con array
+$oppenheimer = new Movie("Oppenheimer", ["biographical", "dramatic", "historical"], 2023, "Cillian Murphy", 20);
 
 // stampo a schermo la variabile statica
 var_dump(Movie::$director);
 
 // stampo a schermo le nuova entità con i valori delle relative proprietà
 var_dump($interstellar, $inception, $oppenheimer);
-
 
 ?>
 
